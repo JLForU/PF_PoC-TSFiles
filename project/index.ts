efd902bb87b66abc601469c5f824e4546e626160
JhosephL <jhosephslizarazom@outlook.com>
2024-10-18 13:53:04 -0500

    Logro lectura de formatos XLSX.

--- project/index.ts
+++ project/index.ts
@@ -11,26 +11,47 @@ function_main() ;
 //// MAIN FUNCTION.
 function function_main ( ) : void {
 
-    let constString_filePath : string = "./try.txt" ;
+    let constString_filePath : string = "./try.xlsx" ;
+
     function_fileExists ( constString_filePath ) ;
-    
+    function_openFile ( constString_filePath ) ;
+
+
     function_print() ;
 
 }
 
 
-//// OTHER FUNCTIONS.
-function function_print ( ) : void {
-    console.log ( "\n\n\tCODE\n" ) ;
-}
-
+//  OTHER FUNCTIONS  //
 //// FILE EXISTING.
 function function_fileExists ( filePath : string ) : void {
-
+    
     fileSystem.access ( filePath , fileSystem.constants.F_OK , (error) => {
-            console.log(`${filePath} ${error ? 'does not exist' : 'exists'}`);
+            console.log ( `${filePath} ${error ? 'does not exist' : 'exists'}` ) ;
         }
     ) ;
 
+}
+
+//// FILE READING.
+function function_openFile ( filePath : string ) : void {
+
+    const path = require("path") ;
+    const xlsx = require("xlsx") ;
+    const filePath_XLSX = path.resolve ( __dirname,filePath ) ;
+    const workbook = xlsx.readFile(filePath_XLSX) ;
+    const sheet = workbook.Sheets[workbook.SheetNames[0]] ;
+
+    console.log ( workbook.SheetNames[0] ) ;
+
+    const cell = sheet["A1"] ;
+
+    console.log ( cell.v ) ;
+
+}
+
+
+function function_print ( ) : void {
+    console.log ( "\n\n\tCODE\n" ) ;
 }
 
